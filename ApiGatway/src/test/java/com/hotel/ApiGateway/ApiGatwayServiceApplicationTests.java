ba7310319c43afea315f1584804610fe347e22fe
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-01-19 12:09:59 +0530

    Initial Commit

---
+++ ApiGatway/src/test/java/com/hotel/ApiGateway/ApiGatwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatwayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
