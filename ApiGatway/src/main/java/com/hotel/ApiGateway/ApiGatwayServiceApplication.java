dd6869762d9e99aea27a6f9388bd0e6d9eab793a
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-03-07 14:05:24 +0530

    Implemented Eureka Server

--- ApiGatway/src/main/java/com/hotel/ApiGateway/ApiGatwayServiceApplication.java
+++ ApiGatway/src/main/java/com/hotel/ApiGateway/ApiGatwayServiceApplication.java
@@ -2,12 +2,12 @@ package com.hotel.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ApiGatwayServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatwayServiceApplication.class, args);
 	}
-
 }
